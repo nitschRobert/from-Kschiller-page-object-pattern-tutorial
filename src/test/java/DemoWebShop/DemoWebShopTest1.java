6c6b195d903a2b7b963eccdb4fd919ada2f94a62
rnicz <nicz.robert@gmail.com>
2020-12-01 19:10:19 +0100

    1.12

--- src/test/java/DemoWebShop/DemoWebShopTest1.java
+++ src/test/java/DemoWebShop/DemoWebShopTest1.java
@@ -1,4 +1,17 @@
 package DemoWebShop;
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DemoWebShopTest1 {
+
+    @Test
+    public void demoShopTest() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.navigate().to("http://demowebshop.tricentis.com");
+
+        driver.close();
+    }
 }
